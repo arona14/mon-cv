3522b4157e8ee2eef5b2d742a9cfb68e7c8695cf
arona14 <sowarona14@gmil.com>
2021-05-17 13:17:59 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Knowledges from './pages/Knowledges';
 import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
 import NotFound from './pages/NotFound';
 
 const App = () => {
